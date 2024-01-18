d250934ba4e7d46a4821fbed74dd4d65a2cbd4dc
Николай <shulyakovskiy@mail.ru>
2024-01-18 15:56:18 +0300

    Improvements of getting started (#73)
    * Bump mysql version. Add more info about authorisation header. Add start db snap with best practices top level element. Modify nginx.conf and add alternative docker compose file
    
    * Review fixes
    
    * Review fixes (part 2)
    
    * Review fixes

---
+++ admin/etc/example-auth.sql
@@ -0,0 +1,11 @@
+--
+-- Dumping data for table `mod_auth`
+--
+
+INSERT INTO `mod_auth` VALUES ('admin','hCG7NSubpIe8M');
+INSERT INTO `mod_auth` VALUES ('beaver','iJdvMl0D40I6M');
+INSERT INTO `mod_auth` VALUES ('hedgehog','6nLwikib1PMAo');
+INSERT INTO `mod_auth` VALUES ('meerkat','uGruJjKihJ7oI');
+INSERT INTO `mod_auth` VALUES ('rabbit','JhDoIGtWEfyrc');
+
+
